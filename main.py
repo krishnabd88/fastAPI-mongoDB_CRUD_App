d8bf5c693895f0d888a015919c6174d4d8dfb79d
Krishna Kumar Shaha <83800917+krishnabd88@users.noreply.github.com>
2024-05-02 13:28:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,24 +12,6 @@ from database import (
 
 # an HTTP-specific exception class  to generate exception information
 
-from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-]
-
-# what is a middleware? 
-# software that acts as a bridge between an operating system or database and applications, especially on a network.
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
